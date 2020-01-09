a8ccf0ebdb68b1d5d12636dbea070b6ea22f4b89
jkamz <jkamz6432@gmail.com>
2020-01-09 22:35:28 +0300

    set up actions

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,1 +1,20 @@
 import axios from 'axios'
+import {
+    API_URL,
+    ONE_PRODUCT,
+    ONE_PRODUCT_SUCCESS,
+    ALL_PRODUCTS,
+    ALL_PRODUCTS_SUCCESS
+} from './constants'
+
+
+export const productActions = {
+    allProducts ({commit}) {
+        commit(ALL_PRODUCTS)
+
+        axios.get(API_URL).then(res => {
+            commit(ALL_PRODUCTS_SUCCESS, res.data)
+        })
+    }
+}
+
--- src/store/constants.js
+++ src/store/constants.js
@@ -6,3 +6,5 @@ export const ALL_PRODUCTS_SUCCESS = 'ALL_PRODUCTS_SUCCESS'
 
 export const ADD_TO_CART = 'ADD_TO_CART'
 export const REMOVE_FROM_CART = 'REMOVE_FROM_CART'
+
+export const API_URL = 'https://e-merse.firebaseio.com/pwa/products.json?orderBy=%22shopid%22&equalTo=%22lavishscents%22'
